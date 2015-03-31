f380d7a21bd7b39bda50ed2783554e8c8c7e024b
midnightapplepie <midnightapplepie@gmail.com>
2015-03-31 12:19:45 -0700

    creagted static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_page/home'
+
+  get 'static_page/help'
+
   resources :microposts
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
