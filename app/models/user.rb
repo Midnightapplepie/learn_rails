399d15cfa94b6667a4b2c95261dc1fa4d065e8d9
midnightapplepie <midnightapplepie@gmail.com>
2015-04-01 15:26:14 -0700

    added user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
 	has_many :microposts
-	validates :name, presence: true
-	validates :email, presence: true
+	has_secure_password
+
+	validates :name, presence: true, length:{maximum:50}
+	valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length:{maximum: 255},
+					  format: {with: valid_email_regex},
+					  uniqueness: true
+	validates :password, length: { minimum: 6 }
 end
